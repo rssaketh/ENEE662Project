ee5e177d2bf90d27c218a844b65dcc8e33dc08d8
rssaketh <rssaketh@gitlab.umiacs.umd.edu>
2017-11-02 17:26:55 -0400

    Added flags to move to different colorspace

--- colorize.m
+++ colorize.m
@@ -1,4 +1,6 @@
-
+close all
+clear all
+params.colorspace = 'HSV'; % or 'LUV' or 'Lab'
 g_name='example.bmp';
 c_name='example_marked.bmp';
 out_name='example_res.bmp';
@@ -12,29 +14,41 @@ cI=double(imread(c_name))/255;
 colorIm=(sum(abs(gI-cI),3)>0.01);
 colorIm=double(colorIm);
 
-sgI=rgb2ntsc(gI);
-scI=rgb2ntsc(cI);
-   
-ntscIm(:,:,1)=sgI(:,:,1);
-ntscIm(:,:,2)=scI(:,:,2);
-ntscIm(:,:,3)=scI(:,:,3);
+if ~strcmp(params.colorspace,'HSV')
+    sgI=rgb2ntsc(gI); %convert to LUV Space
+    scI=rgb2ntsc(cI);
+    ntscIm(:,:,1)=sgI(:,:,1);
+    ntscIm(:,:,2)=scI(:,:,2);
+    ntscIm(:,:,3)=scI(:,:,3);
+else
+    sgI = rgb2hsv(gI);
+    scI = rgb2hsv(cI);
+    ntscIm(:,:,1) = scI(:,:,1);
+    ntscIm(:,:,2) = scI(:,:,2);
+    ntscIm(:,:,3) = sgI(:,:,3);
+
+
+end
+
 
 
 max_d=floor(log(min(size(ntscIm,1),size(ntscIm,2)))/log(2)-2);
 iu=floor(size(ntscIm,1)/(2^(max_d-1)))*(2^(max_d-1));
 ju=floor(size(ntscIm,2)/(2^(max_d-1)))*(2^(max_d-1));
 id=1; jd=1;
+% figure,imshow(colorIm)
 colorIm=colorIm(id:iu,jd:ju,:);
+% figure,imshow(colorIm)
 ntscIm=ntscIm(id:iu,jd:ju,:);
 
 if (solver==1)
   nI=getVolColor(colorIm,ntscIm,[],[],[],[],5,1);
   nI=ntsc2rgb(nI);
 else
-  nI=getColorExact(colorIm,ntscIm);
+  [nI,snI] =getColorExact(colorIm,ntscIm,params);
 end
-
 figure, imshow(nI)
+% figure, imshow(nI)
 
 imwrite(nI,out_name)
    
--- getColorExact.m
+++ getColorExact.m
@@ -1,30 +1,36 @@
-function [nI,snI]=getColorExact(colorIm,ntscIm)
+function [nI,snI]=getColorExact(colorIm,ntscIm, params)
 
 n=size(ntscIm,1); m=size(ntscIm,2);
 imgSize=n*m;
 
-
-nI(:,:,1)=ntscIm(:,:,1);
-
+if ~strcmp(params.colorspace, 'HSV')
+    nI(:,:,1)=ntscIm(:,:,1);
+    comp_temp = nI(:,:,1);
+else
+    nI(:,:,3) = ntscIm(:,:,3);
+    comp_temp = nI(:,:,3);
+end
 indsM=reshape([1:imgSize],n,m);
-lblInds=find(colorIm);
+lblInds=find(colorIm); % find where the marks have been given
 
 wd=1; 
 
 len=0;
 consts_len=0;
-col_inds=zeros(imgSize*(2*wd+1)^2,1);
+col_inds=zeros(imgSize*(2*wd+1)^2,1); %3*3 times the image size for affinity matrix
 row_inds=zeros(imgSize*(2*wd+1)^2,1);
 vals=zeros(imgSize*(2*wd+1)^2,1);
-gvals=zeros(1,(2*wd+1)^2);
+gvals=zeros(1,(2*wd+1)^2);  %3x3 window around a non constraint?
 
+%%%%%%%%%%%%%%%%%%% Construct the affinity matrix %%%%%%%%%%%%%%%%%%%%%%%
 
-for j=1:m
-   for i=1:n
+for j=1:m  %All cols
+   for i=1:n %All rows
       consts_len=consts_len+1;
       
-      if (~colorIm(i,j))   
+      if (~colorIm(i,j))   %if not a constraint
         tlen=0;
+        % Check in the neighbourhood of the current pixel
         for ii=max(1,i-wd):min(i+wd,n)
            for jj=max(1,j-wd):min(j+wd,m)
             
@@ -32,25 +38,32 @@ for j=1:m
                  len=len+1; tlen=tlen+1;
                  row_inds(len)= consts_len;
                  col_inds(len)=indsM(ii,jj);
-                 gvals(tlen)=ntscIm(ii,jj,1);
+                 % Getting intensities to calculate the affinity matrix
+                 gvals(tlen)=comp_temp(ii,jj); %ntscIm(ii,jj,1);
+%                    gvals(tlen) = ntscIm(ii,jj,3);
               end
            end
         end
-        t_val=ntscIm(i,j,1);
+        % Intensity at current pixel
+        t_val= comp_temp(i,j); %ntscIm(i,j,1);
+%         t_val = ntscIm(i,j,3);
         gvals(tlen+1)=t_val;
         c_var=mean((gvals(1:tlen+1)-mean(gvals(1:tlen+1))).^2);
         csig=c_var*0.6;
         mgv=min((gvals(1:tlen)-t_val).^2);
         if (csig<(-mgv/log(0.01)))
-	   csig=-mgv/log(0.01);
-	end
-	if (csig<0.000002)
-	   csig=0.000002;
+            csig=-mgv/log(0.01);
         end
-
+        if (csig<0.000002)
+            csig=0.000002;
+        end
+        % Exponential weight functions
+        % if intensity is closer, more weight, else less weight
         gvals(1:tlen)=exp(-(gvals(1:tlen)-t_val).^2/csig);
+        % Normalized to 1
         gvals(1:tlen)=gvals(1:tlen)/sum(gvals(1:tlen));
-        vals(len-tlen+1:len)=-gvals(1:tlen);
+        % Not Clear why
+        vals(len-tlen+1:len) = -gvals(1:tlen);
       end
 
         
@@ -62,25 +75,40 @@ for j=1:m
    end
 end
 
-       
+%%%%%%%%%%%%%%%%% End of construction of affinity matrix %%%%%%%%%%%%%%
 vals=vals(1:len);
 col_inds=col_inds(1:len);
 row_inds=row_inds(1:len);
 
-
+%Constructing the affinity matrix.
 A=sparse(row_inds,col_inds,vals,consts_len,imgSize);
 b=zeros(size(A,1),1);
 
-
-for t=2:3
+if ~strcmp(params.colorspace,'HSV')
+    range = 2:3;
+else
+    range = 1:2;
+end
+for t=range
     curIm=ntscIm(:,:,t);
     b(lblInds)=curIm(lblInds);
     new_vals=A\b;   
     nI(:,:,t)=reshape(new_vals,n,m,1);    
 end
 
+% for t=2:3
+%     curIm=ntscIm(:,:,t);
+%     b(lblInds)=curIm(lblInds);
+%     new_vals=A\b;   
+%     nI(:,:,t)=reshape(new_vals,n,m,1);    
+% end
+
 
 
 snI=nI;
-nI=ntsc2rgb(nI);
+if ~strcmp(params.colorspace,'HSV')
+    nI=ntsc2rgb(nI);
+else
+    nI = hsv2rgb(nI);
+end
 
