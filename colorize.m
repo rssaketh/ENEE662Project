a86af19217ee2e2aec0383f0d8179eb54bbf9b5d
Ananth H <hariananth92@gmail.com>
2017-11-14 19:52:40 -0500

    init commit

--- colorize.m
+++ colorize.m
@@ -1,6 +1,6 @@
 close all
 clear all
-params.colorspace = 'HSV'; % or 'LUV' or 'Lab'
+params.colorspace = 'LUV'; % 'HSV' or 'LUV' or 'Lab'
 g_name='example.bmp';
 c_name='example_marked.bmp';
 out_name='example_res.bmp';
@@ -17,9 +17,9 @@ colorIm=double(colorIm);
 if ~strcmp(params.colorspace,'HSV')
     sgI=rgb2ntsc(gI); %convert to LUV Space
     scI=rgb2ntsc(cI);
-    ntscIm(:,:,1)=sgI(:,:,1);
-    ntscIm(:,:,2)=scI(:,:,2);
-    ntscIm(:,:,3)=scI(:,:,3);
+    ntscIm(:,:,1)=sgI(:,:,1); % luminance
+    ntscIm(:,:,2)=scI(:,:,2); % chrominance 1 - u
+    ntscIm(:,:,3)=scI(:,:,3); % chrominance 1 - v
 else
     sgI = rgb2hsv(gI);
     scI = rgb2hsv(cI);
@@ -33,10 +33,12 @@ end
 
 
 max_d=floor(log(min(size(ntscIm,1),size(ntscIm,2)))/log(2)-2);
+% TODO = what are iu, ju?
 iu=floor(size(ntscIm,1)/(2^(max_d-1)))*(2^(max_d-1));
 ju=floor(size(ntscIm,2)/(2^(max_d-1)))*(2^(max_d-1));
 id=1; jd=1;
 % figure,imshow(colorIm)
+% TODO - why are they disregarding the 3rd channel? Isn't that v?
 colorIm=colorIm(id:iu,jd:ju,:);
 % figure,imshow(colorIm)
 ntscIm=ntscIm(id:iu,jd:ju,:);
